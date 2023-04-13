86e1e5e3d826ef3bbdae89bd5de3c267f499ff6b
RaphaelMacedoB <lucasmacedoes@gmail.com>
2023-04-13 16:05:14 -0300

    feat: :construction: add the city model without associations

---
+++ src/models/City.js
@@ -0,0 +1,15 @@
+import { DataTypes, Model } from "sequelize";
+class City extends Model {
+    static init(sequelize) {
+        super.init({
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: "O nome não pode ser nulo!" },
+                    notEmpty: { msg: "O nome não pode ser vazio!" }
+                }
+            }
+        }, { sequelize, modelName: "city", tableName: "cities" });
+    }
+}
