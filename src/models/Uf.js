b6ee05051e4b6ec350d85b2590cec64d9a00ca57
RaphaelMacedoB <lucasmacedoes@gmail.com>
2023-04-13 15:28:05 -0300

    docs: :memo: add class diagram

--- src/models/Uf.js
+++ src/models/Uf.js
@@ -1,26 +1,24 @@
 import { DataTypes, Model } from "sequelize";
-import { } from sequelize;
-
 class Uf extends Model {
-  static init(sequelize) {
-    super.init({
-      sigla: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: { msg: "O nome não pode ser nulo!" },
-          notEmpty: { msg: "O nome não pode ser vazio!" },
-          len: { args: [2, 2], msg: "A sigla deve possuir 2 caracteres!" }
-        }
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: { msg: "O nome não pode ser nulo!" },
-          notEmpty: { msg: "O nome não pode ser vazio!" }
-        }
-      }
-    }, { sequelize, modelName: "uf", tableName: "ufs" });
-  }
+    static init(sequelize) {
+        super.init({
+            sigla: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: "O nome não pode ser nulo!" },
+                    notEmpty: { msg: "O nome não pode ser vazio!" },
+                    len: { args: [2, 2], msg: "A sigla deve possuir 2 caracteres!" }
+                }
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: "O nome não pode ser nulo!" },
+                    notEmpty: { msg: "O nome não pode ser vazio!" }
+                }
+            }
+        }, { sequelize, modelName: "uf", tableName: "ufs" });
+    }
 }
