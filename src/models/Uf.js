ae8bcf685d73d147d87f7035e4bdf74b0473c2e5
Jefferson Abreu <jehffersson@hotmail.com>
2023-04-13 12:01:05 -0300

    feat: :sparkles: add new model UF

---
+++ src/models/Uf.js
@@ -0,0 +1,26 @@
+import { DataTypes, Model } from "sequelize";
+import { } from sequelize;
+
+class Uf extends Model {
+  static init(sequelize) {
+    super.init({
+      sigla: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "O nome não pode ser nulo!" },
+          notEmpty: { msg: "O nome não pode ser vazio!" },
+          len: { args: [2, 2], msg: "A sigla deve possuir 2 caracteres!" }
+        }
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "O nome não pode ser nulo!" },
+          notEmpty: { msg: "O nome não pode ser vazio!" }
+        }
+      }
+    }, { sequelize, modelName: "uf", tableName: "ufs" });
+  }
+}
