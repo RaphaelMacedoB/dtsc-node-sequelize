2f8b920f5eab580a3973da8952c051ee29fc4184
RaphaelMacedoB <lucasmacedoes@gmail.com>
2023-04-14 00:03:46 -0300

    feat: :sparkles: add Tutor model

--- src/config/database-connections.js
+++ src/config/database-connections.js
@@ -4,19 +4,19 @@ import { databaseConfig } from "./database-config.js";
 import { Uf } from "../models/Uf.js";
 import { City } from "../models/City.js";
 import { District } from "../models/District.js";
-
+import { Tutor } from "../models/Tutor.js"
 const sequelize = new Sequelize(databaseConfig)
 Uf.init(sequelize)
 City.init(sequelize)
 District.init(sequelize)
-
+Tutor.init(sequelize)
 databaseInserts();
 
 function databaseInserts() {
     (async() => {
         await sequelize.sync({ force: true })
-        const city1 = await City.create({ name: "Rio de Janeiro" })
-        console.log(city1.name);
+        const tutor1 = await Tutor.create({ name: "Lucas Macedo", house_number: "10" })
+        console.log(`Sou ${tutor1.name} e moro na casa de número ${tutor1.house_number}`);
     })();
 }
 
---
+++ src/models/Tutor.js
@@ -0,0 +1,66 @@
+import { DataTypes, Model } from "sequelize";
+
+class Tutor extends Model {
+    static init(sequelize) {
+        super.init({
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: "O nome não pode ser nulo!" },
+                    notEmpty: { msg: "O nome não pode ser vazio!" }
+                }
+            },
+            house_number: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: "O número não pode ser nulo!" },
+                    notEmpty: { msg: "O número não pode ser vazio!" },
+                    min: { args: [0], msg: "Digite um número positivo!" }
+                }
+            },
+            zip_code: DataTypes.BIGINT,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "O CEP não pode ser nulo!" },
+                notEmpty: { msg: "O CEP não pode ser vazio!" },
+                len: { args: [0, 8], msg: "Digite apenas oito números!" }
+            },
+            email: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "O CEP não pode ser nulo!" },
+                notEmpty: { msg: "O CEP não pode ser vazio!" },
+                isEmail: { args: [true], msg: "Digite um email válido!" }
+            },
+            phone: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "O número de telefone não pode ser nulo!" },
+                notEmpty: { msg: "O número de telefone não pode ser vazio!" },
+            },
+            public_place: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "O termo não pode ser nulo!" },
+                notEmpty: { msg: "O termo de telefone não pode ser vazio!" },
+            },
+            date_of_birth: DataTypes.DATEONLY,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "O termo não pode ser nulo!" },
+                notEmpty: { msg: "O termo de telefone não pode ser vazio!" },
+            },
+            space_size: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "O valor do espaço disponível não pode ser nulo!" },
+                notEmpty: { msg: "O valor do espaço disponível não pode ser vazio!" },
+                len: { args: [0, Infinity], msg: "Não existe espaço negativo! Digite um valor positivo." }
+            }
+        }, { sequelize, modelName: "tutor", tableName: "tutors" });
+    }
+}
+
+export { Tutor };
