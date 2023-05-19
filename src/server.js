de41a76ab8475f9869ada04b15f0e918a4bde111
RaphaelMacedoB <72576633+RaphaelMacedoB@users.noreply.github.com>
2023-05-19 09:55:47 -0300

    feat: add error-handler.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import sequelize from "./config/database-connections.js"; //SQLITE
 import routes from "./routes.js";
 //import sequelize from "./config/database-connections-pg.js" // POSTGRES
+import errorHandler from '../src/_middleware/error-handler.js';
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-
+app.use(errorHandler);
 
 app.listen(3333);
