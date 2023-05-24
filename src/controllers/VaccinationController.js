13146dcc89d8a175f549a7adb4b702288049826b
lucasmacedobernardino <lucasmacedoes@gmail.com>
2023-05-24 19:15:50 -0300

    feat: vaccination controller and service

---
+++ src/controllers/VaccinationController.js
@@ -0,0 +1,39 @@
+import { VaccinationService } from "../services/VaccinationService.js";
+/**
+ * @author Lucas Macedo Bernardino
+ */
+class VaccinationController {
+
+    static async findAll(req, res, next) {
+        VaccinationService.findAll()
+            .then(objs => res.json(objs))
+            .catch(next);
+    }
+
+    static async findByPk(req, res, next) {
+        VaccinationService.findByPk(req)
+            .then(obj => res.json(obj))
+            .catch(next);
+    }
+
+    static async create(req, res, next) {
+        VaccinationService.create(req)
+            .then(obj => res.json(obj))
+            .catch(next);
+    }
+
+    static async update(req, res, next) {
+        VaccinationService.update(req)
+            .then(obj => res.json(obj))
+            .catch(next);
+    }
+
+    static async delete(req, res, next) {
+        VaccinationService.delete(req)
+            .then(obj => res.json(obj))
+            .catch(next);
+    }
+
+}
+
+export { VaccinationController };
