9a591728137108085ffa0ea4fb816b22d73b8d1f
Lucas Macedo Bernardino <lucasmacedoes@gmail.com>
2023-05-26 12:31:01 -0300

    test: add vaccination rules

--- testeRegraVacinacao.js
+++ testeRegraVacinacao.js
@@ -22,3 +22,22 @@ if (todayDate > datePlusInterval){
   console.log('\nO cachorro não pode receber vacina !!');
 }
 
+
+
+// static async findVaccinationByVaccineAndDog(req) {
+//     const { dog, vaccine } = req.params;
+//     const vaccineId = vaccine.id;
+//     const dogId = dog.id;
+//     const objs = await sequelize.query("SELECT vc.id, vc.date, v.dosage_interval_days FROM Vaccinations vc JOIN Vaccines v ON vc.vaccine_id = v.id WHERE vc.dog_id = :dogId AND vc.vaccine_id = :vaccineId ORDER BY vc.id DESC LIMIT 1", { replacements: { dog_id: dogId, vaccine_id: vaccineId }, type: QueryTypes.SELECT });
+//     return objs;
+//   }
+                                                  //SELECT * FROM Vaccinations, Vaccines WHERE dog_id = :dogId AND vaccine_id = :vaccineId"
+                                                  //SELECT vc.id, vc.date, v.dosage_interval_days FROM Vaccinations vc JOIN Vaccines v ON vc.vaccine_id = v.id WHERE vc.dog_id = 1 AND vc.vaccine_id = 1 ORDER BY vc.id DESC LIMIT 1
+
+
+
+    //                                               // Regra de Negócio 1: Para vacinar é necessário respeitar os intervalos de dose de cada vacina.
+    // const vaccination = await vaccinationService.findVaccinationByVaccineAndDog(req);
+    // const vaccinationData = vaccination.date;
+    // const vaccineIntervalDays = vaccinationLast.dosage_interval_days;
+    // const todayDate = new Date();
